f2d75761f11055a1305cc5cc840bb4895914295d
shubhamgyd <shubhamyadav97660@gmail.com>
2023-02-04 00:10:28 +0530

    commit

--- app/src/main/java/com/example/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreet/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.birthdaygreet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+        fun createBirthdayCard() {
+
+            Toast.makeText(this,"Button was clicked", Toast.LENGTH_LONG).show()
+
+    }
 }
+
