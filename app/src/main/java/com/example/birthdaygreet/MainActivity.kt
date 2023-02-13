44ee6cfa997d3d788e0a1f233e399421f8ebbb1b
shubhamgyd <shubhamyadav97660@gmail.com>
2023-02-13 15:31:00 +0530

    Create button behaviour

--- app/src/main/java/com/example/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreet/MainActivity.kt
@@ -2,17 +2,25 @@ package com.example.birthdaygreet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val editText: EditText = findViewById(R.id.editText)
+    fun createBirthdayCard(view: View) {
+
+        val enteredData: String = editText.text.toString()
+
+
+        Toast.makeText(this, enteredData, Toast.LENGTH_LONG).show()
     }
-        fun createBirthdayCard() {
-
-            Toast.makeText(this,"Button was clicked", Toast.LENGTH_LONG).show()
-
     }
 }
 
